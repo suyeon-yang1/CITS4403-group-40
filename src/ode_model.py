99d796442f6138bf83a8eecaafcc0e486f269056
su <didtndus1187@gmail.com>
2025-10-13 20:54:06 +0800

    Fix circular import

--- src/ode_model.py
+++ src/ode_model.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import numpy as np
+import matplotlib.pyplot as plt
 
 @dataclass
 class ODEParams:
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,20 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from .helper_functions import replicate_abm, mean_ci, plot_with_ci, extinction_prob
+from .seed import set_seed
 
-from src.ode_model import ODEParams, lotka_volterra
-from src.abm_model import ABMParams, run_abm
-from utils.helper_functions import (
-    replicate_abm,
-    mean_ci,
-    plot_with_ci,
-    extinction_prob
-)
-from utils.seed import set_seed
-
-__all__ = [
-    "np", "plt",
-    "ODEParams", "lotka_volterra",
-    "ABMParams", "run_abm",
-    "replicate_abm", "mean_ci", "plot_with_ci", "extinction_prob",
-    "set_seed"
-]
+__all__ = ["replicate_abm", "mean_ci", "plot_with_ci", "extinction_prob", "set_seed"]
